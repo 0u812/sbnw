588d486605dc38f368ca9ee8ffb0057e4349433d
0u812 <0u812@github.com>
2015-04-08 14:41:00 -0700

    create about box, fix #13

--- sandbox/python/pyfab_app.py
+++ sandbox/python/pyfab_app.py
@@ -311,7 +311,8 @@ class Autolayout(MainWindowBaseClass):
         self.homepageAct = QAction(QtGui.QIcon(self.getIconPath('32x32/homepage.png')), '&Homepage', self)
         self.homepageAct.triggered.connect(self.openhomepageEvent)
 
-        self.aboutAct = QAction(QtGui.QIcon(self.getIconPath('32x32/help-about-2.png')), '&About', self)
+        self.aboutAct = QAction(QtGui.QIcon(self.getIconPath('32x32/help-about-2.png')), '&About/License', self)
+        self.aboutAct.triggered.connect(self.openAboutBoxEvent)
 
         # Emit default triggers
         # Make select tool default
@@ -670,6 +671,20 @@ class Autolayout(MainWindowBaseClass):
 
     def openhomepageEvent(self, event):
         QtGui.QDesktopServices.openUrl(QtCore.QUrl('https://github.com/0u812/sbnw'))
+
+    def openAboutBoxEvent(self, event):
+      box = QMessageBox.about(self, 'SBNW: SBML Network Viewer',
+                              '<center><h1>SBNW</h1>\n\n<h2>Network Viewer for SBML</h2>\n<a href=\"https://github.com/0u812/sbnw\">Visit Homepage</a>\n</center>\n<br/>' +
+'''<strong>Copyright (c) 2015, J. Kyle Medley, Herbert M. Sauro</strong> <br/>
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+<ul>
+    <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>
+    <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+    <li>Neither the name of SBNW nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</li>
+</ul>
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''')
 
 if not inspyder:
   FrameBaseClass = QtWidgets.QFrame
