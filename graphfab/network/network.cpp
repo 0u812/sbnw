00ae45781909b3499c5e4a4baae8bcda1d45c43f
0u812 <0u812@github.com>
2015-04-15 16:49:35 -0700

    try to infer reaction centroid from curves

--- graphfab/network/network.cpp
+++ graphfab/network/network.cpp
@@ -1738,11 +1738,11 @@ namespace Graphfab {
             // calculate the centroid
             ::Curve const* curve = rg->getCurve();
 //             if (!curve)
-//               std::cerr << "no curve\n";
+//               std::cerr << rg->getId() << ": no curve\n";
 //             else
-//               std::cerr << "have curve, n segs = " << curve->getNumCurveSegments() << "\n";
+//               std::cerr << rg->getId() << ": have curve, n segs = " << curve->getNumCurveSegments() << "\n";
             if (curve && curve->getNumCurveSegments() > 0) {
-                // use preexisting coords
+                // use preexisting coords via reaction curve
 //                 std::cerr << "manual centroid\n";
                 r->setCentroid(curve->getCurveSegment(0)->getEnd()->x(), curve->getCurveSegment(0)->getEnd()->y());
 
@@ -1767,8 +1767,33 @@ namespace Graphfab {
                 r->recalcCurveCPs();
 
                 r->clearDirtyFlag();
-            } else
+            } else {
+                // try to get the centroid from the reaction curves
+                for(int i_spc=0; i_spc<rg->getNumSpeciesReferenceGlyphs(); ++i_spc) {
+                    // spec ref
+                    const SpeciesReferenceGlyph* srg = rg->getSpeciesReferenceGlyph(i_spc);
+                    // role
+                    RxnRoleType role = SBMLRole2GraphfabRole(srg->getRole());
+                    // curve
+                    ::Curve const* crv = srg->getCurve();
+                    if (!crv)
+                      break;
+
+                    for (int i_seg=0; i_seg<crv->getNumCurveSegments(); ++i_seg) {
+                      ::LineSegment const* seg = crv->getCurveSegment(i_seg);
+                      if (role == RXN_ROLE_PRODUCT || role == RXN_ROLE_SIDEPRODUCT)
+                        r->setCentroid(seg->getStart()->x(), seg->getStart()->y());
+                      else
+                        r->setCentroid(seg->getEnd()->x(), seg->getEnd()->y());
+                      goto skip_recalc_centroid;
+                    }
+                }
+
+                // if all else fails average node coords
                 r->forceRecalcCentroid();
+
+                skip_recalc_centroid:;
+            }
 
 //             r->rebuildCurves();
 //             std::cerr << "Reaction centroid: " << r->getCentroid(NetworkElement::COORD_SYSTEM_GLOBAL) << "\n";
